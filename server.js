226c006cefe5907e247109ca66715736c5fb76f3
momo <momodev19@gmail.com>
2023-05-18 21:41:59 -0700

    keep alive

--- server.js
+++ server.js
@@ -8,11 +8,13 @@ const wss = new WebSocket.Server({ server })
 
 wss.on('connection', (ws) => {
     ws.on('message', (data) => {
-        wss.clients.forEach((client) => {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(data.toString());
-            }
-        })
+        if ('ping' !== data.toString()) {
+            wss.clients.forEach((client) => {
+                if (client !== ws && client.readyState === WebSocket.OPEN) {
+                    client.send(data.toString());
+                }
+            })
+        }
     })
 })
 
